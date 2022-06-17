dc3332016e5b3cbc7a5f1e41d4af1e62a2b34c2a
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-06-18 00:02:58 +0530

    Time: 35 ms (5.35%), Space: 74.8 MB (5.13%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Set<Integer> set = new HashSet();
+        
+        for(int x:nums)
+            set.add(x);
+        
+        if(set.size() != nums.length)
+            return true;
+        else 
+            return false;
+    }
+}
