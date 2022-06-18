399c3c403480800376d7df171557b70d208568a1
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-06-19 01:17:34 +0530

    Time: 122 ms (30.05%), Space: 43.8 MB (96.35%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,17 +3,34 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    let str=s.toLowerCase();
     
-   let arr = []
-   
-   for(let i=0; i<s.length; i++){
-       if((s[i] >= "A" && s[i]  <= "Z") || (s[i] >= "a" && s[i] <= "z") || (s[i] >= "0" && s[i] <= "9")){
-           arr.push(s[i].toLowerCase())
-       } 
-   }
+    if(str.length == 1) return true;
     
-    let seedha = arr.join("");
-    let ulta = arr.reverse().join("")
-    return seedha == ulta;
+    let low = 0;
+    let high = str.length - 1;
+
+    while(low <= high){
+        
+        let lower = str[low]
+        let upper = str[high]
+        
+        if(   ! ((lower >= "a" && lower <= "z") ||  (lower >= "0" && lower <= "9"))){
+           low++; 
+        }
+        
+        else if(   !  (  (upper >= "a" && upper <= "z")  ||  (upper >= "0" && upper <= "9"))){
+           high--; 
+        } 
+        
+        else if(lower == upper){
+            low++;
+            high--;
+        } 
+        
+        else 
+             return false;   
+    }
     
+    return true;
 };
