8089a0fe79c86435990bae09fcc30c4b70a499f3
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-06-21 18:12:13 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (81.03%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -19,11 +19,9 @@ class Solution {
         if(p == null && q == null)        
             return true;
         
-        if(p == null && q != null)
-            return false;
-        
-        if(q == null && p != null)
-            return false;
+        if(p == null) return false;
+       
+        if(q == null) return false;
         
         if( p.val == q.val ){
            return  isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
