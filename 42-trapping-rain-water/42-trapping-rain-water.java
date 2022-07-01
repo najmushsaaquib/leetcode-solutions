18b1889439133268fe7dd19a3255360cac4f48e4
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-07-01 16:28:26 +0530

    Time: 2 ms (55.78%), Space: 49.1 MB (21.02%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,56 +1,32 @@
 class Solution {
-    public int trap(int[] height) {
+    public int trap(int[] arr) {
         
-        int n = height.length;
-        
+        int n = arr.length;
         if(n < 3)
             return 0;
         
         int trappedWater = 0;
         
-        int[] leftMax = leftMax(height);
-        int[] rightMax = rightMax(height);
-        
-        for(int i=0; i<n; i++){
-            
-            int min = Math.min(leftMax[i], rightMax[i]);
-            trappedWater += min-height[i];
-        }
-        
-        return trappedWater;
-        
-    }
-    
-    // Helper method to preCompute Max values to the left;
-    static int[] leftMax(int[] arr){
-        
-        int n = arr.length;
         int[] leftMax = new int[n];
-        leftMax[0] = arr[0];
-            
-        for(int i=1; i<n; i++){
-            int max = Math.max(arr[i], leftMax[i-1]);
-            leftMax[i] = max;
-            
-        }
-        
-        return leftMax;
-    }
-    
-    // Helper method to preCompute Max values to the right;
-    static int[] rightMax(int[] arr){
-        
-        int n = arr.length;
         int[] rightMax = new int[n];
-        rightMax[n-1] = arr[n-1];
-            
-        for(int i=n-2; i>=0; i--){
-            int max = Math.max(arr[i], rightMax[i+1]);
-            rightMax[i] = max;
-            
-        }
         
-        return rightMax;
+        //PreComputing Max values to the left;
+        leftMax[0] = arr[0];
+        for(int i=1; i<n; i++)
+            leftMax[i] = Math.max(arr[i], leftMax[i-1]);
+        
+         //PreComputing Max values to the right;
+         rightMax[n-1] = arr[n-1];
+         for(int i=n-2; i>=0; i--)
+            rightMax[i] = Math.max(arr[i], rightMax[i+1]);
+        
+         //Computing how much water can be trapped
+         for(int i=0; i<n; i++){
+            int min = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += min-arr[i];
+         }
+        
+         return trappedWater;
     }
     
 }
